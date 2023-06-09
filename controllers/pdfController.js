8fb5ee7062b3f5155741c798bd7e4e20e7baffdc
Wei Chin Neoh <weichin@gmail.com>
2023-06-09 14:59:01 +0800

    added code to parse pdf

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -26,7 +26,6 @@ export const parsePdf = (req, res) => {
         res.status(500).send({ error: "Error fetching PDF from URL" });
     });
 
-/*
     // Specify the file path
 const filePath = './pdf_trans.json';
 
@@ -48,23 +47,111 @@ fs.readFile(filePath, 'utf8', (err, data) => {
 
     const jsonArray = jsonData.Pages;
 
+    const oriTests = [
+        'WBC',
+        'RBC',
+        'Hemoglobin',
+        'Hematocrit',
+        'MCV',
+        'MCH', 
+        'MCHC',  
+        'RDW - CV',  
+        'Platelet',  
+        'Neutrophils', 
+        'Lymphocytes',  
+        'Monocytes',  
+        'Eosinophils',  
+        'Basophils - %',  
+        'Absolute Neutrophils',  
+        'Absolute Lymphocytes',  
+        'Absolute Monocytes',  
+        'Absolute Eosinophils',  
+        'Basophils',
+        'Glucose',
+        'BUN',
+        'Creatinine',
+        'eGFR (Non-African American)',
+        'eGFR (African American)',
+        'BUN/Creatinine Ratio',
+        'Sodium',
+        'Potassium',
+        'Chloride',
+        'Bicarbonate',
+        'Calcium',
+        'Total Protein',
+        'Albumin',
+        'Globulin',
+        'Albumin/Globulin Ratio',
+        'Total Bilirubin',
+        'Direct Bilirubin',
+        'AST (Aspartate aminotransferase)',
+        'ALT (Alanine aminotransferase)',
+        'ALP (Alkaline Phosphatase)',
+        'GGT',
+        'HbA1c, Glycosylated Hemoglobin',
+        'Insulin',
+        'Uric Acid',
+        'Cholesterol, Total',
+        'Triglycerides',
+        'HDL, Cholesterol',
+        'LDL Calculated',
+        'Cholesterol/HDL Ratio',
+        'Lipoprotein (a)',
+        'Homocysteine',
+        'C-Reactive Protein, Quant',
+        'TSH-DXI',
+        'Free T4 - DXI',
+        'Free T3 - DXI',
+        'Thyroglobulin Antibodies',
+        'Thyroid Peroxidase (TPO) Ab - DXI',
+        'Prolactin - DXI',
+        'LH - DXI',
+        'FSH - DXI',
+        'DHEA-Sulfate - DXI',
+        'Testosterone - DXI',
+        'Free Testosterone',
+        'Bioavailable Testosterone',
+        'SHBG',
+        'Estrone-MSS',
+        'Pregnenolone- MSS',
+        'Progesterone - DXI',
+        'IGF-1',
+        'Cortisol - DXI',
+        'Estradiol - DXI',
+        'Vitamin D - DXI',
+        'Ferritin',
+        'Vitamin B12, Diluted - DXI',
+        'Folate, Serum - DXI',
+    ];
     // Iterate over the JSON data
     for (const obj of jsonArray) {
-        console.log(obj);
-    const testNameMatch = obj.Texts.R[0].T.match(testNameRegex);
-    const resultMatch = obj.Texts.R[0].S.toString().match(resultRegex);
+        const tests = obj.Texts;
 
-    // Extracted test name and result
-    const testName = testNameMatch ? testNameMatch[1] : '';
-    const result = resultMatch ? resultMatch[1] : '';
+        const isDecimalOrNumberWithRange = /^-?\d+(\.\d+)?|<[0-9]+|>[0-9]+$/;
+        tests.forEach((test,idx,array) => {
+            const testName = decodeURIComponent(test.R[0].T);
+            const next = array[idx + 1] || null;
 
-    console.log('Test Name:', testName);
-    console.log('Result:', result);
+            if ((/Result/).test(testName)) {
+                console.log("---------");
+            }
+            //console.log(testName);
+            if (oriTests.includes(testName)) {
+                const point = next.R[0].T;
+
+                if (isDecimalOrNumberWithRange.test(point)){
+                    console.log(`${testName} : ${point}`);
+                }
+
+            }
+        });
+
+  
     }
 
   } catch (error) {
     console.error('Error parsing JSON:', error);
   }
-});*/
+});
 
 };
