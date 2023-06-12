30fe55a46e38d45e2446515203db7b1495b3449e
shuaiwang <shuaiwang11235@gmail.com>
2023-06-12 00:09:14 -0400

    parse api

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -6,8 +6,7 @@ export const parsePdf = (req, res) => {
     const pdfParser = new PDFParser();
     const { url : pdfUrl } = req.body;
     pdfParser.on('pdfParser_dataReady',  pdfData => {
-        fs.writeFile("./pdf_trans.json", JSON.stringify(pdfData), {}, ()=>{});
-        res.status(200).send(JSON.stringify(pdfData)); // Move the response here
+        res.status(200).send(generateResponse(JSON.parse(JSON.stringify(pdfData)))); // Move the response here
     });
 
     pdfParser.on('pdfParser_dataError',  errData => {
@@ -25,133 +24,115 @@ export const parsePdf = (req, res) => {
         console.error("Error fetching PDF from URL:", error);
         res.status(500).send({ error: "Error fetching PDF from URL" });
     });
+};
 
-    // Specify the file path
-const filePath = './pdf_trans.json';
+const generateResponse = (jsonData) => {
+    const result = [];
+    try {
+        const jsonArray = jsonData.Pages;
 
-// Read the file content
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading file:', err);
-    return;
-  }
+        const oriTests = [
+            'WBC',
+            'RBC',
+            'Hemoglobin',
+            'Hematocrit',
+            'MCV',
+            'MCH',
+            'MCHC',
+            'RDW - CV',
+            'Platelet',
+            'Neutrophils',
+            'Lymphocytes',
+            'Monocytes',
+            'Eosinophils',
+            'Basophils - %',
+            'Absolute Neutrophils',
+            'Absolute Lymphocytes',
+            'Absolute Monocytes',
+            'Absolute Eosinophils',
+            'Basophils',
+            'Glucose',
+            'BUN',
+            'Creatinine',
+            'eGFR (Non-African American)',
+            'eGFR (African American)',
+            'BUN/Creatinine Ratio',
+            'Sodium',
+            'Potassium',
+            'Chloride',
+            'Bicarbonate',
+            'Calcium',
+            'Total Protein',
+            'Albumin',
+            'Globulin',
+            'Albumin/Globulin Ratio',
+            'Total Bilirubin',
+            'Direct Bilirubin',
+            'AST (Aspartate aminotransferase)',
+            'ALT (Alanine aminotransferase)',
+            'ALP (Alkaline Phosphatase)',
+            'GGT',
+            'HbA1c, Glycosylated Hemoglobin',
+            'Insulin',
+            'Uric Acid',
+            'Cholesterol, Total',
+            'Triglycerides',
+            'HDL, Cholesterol',
+            'LDL Calculated',
+            'Cholesterol/HDL Ratio',
+            'Lipoprotein (a)',
+            'Homocysteine',
+            'C-Reactive Protein, Quant',
+            'TSH-DXI',
+            'Free T4 - DXI',
+            'Free T3 - DXI',
+            'Thyroglobulin Antibodies',
+            'Thyroid Peroxidase (TPO) Ab - DXI',
+            'Prolactin - DXI',
+            'LH - DXI',
+            'FSH - DXI',
+            'DHEA-Sulfate - DXI',
+            'Testosterone - DXI',
+            'Testosterone - DXI M/F SUPP',
+            'Free Testosterone',
+            'Bioavailable Testosterone',
+            'SHBG',
+            'Estrone-MSS',
+            'Pregnenolone- MSS',
+            'Progesterone - DXI',
+            'IGF-1',
+            'Cortisol - DXI',
+            'Estradiol - DXI',
+            'Vitamin D - DXI',
+            'Ferritin',
+            'Vitamin B12, Diluted - DXI',
+            'Vitamin B12 - DXI',
+            'Folate, Serum - DXI',
+            'PSA, Total - DXI'
+        ];
+        // Iterate over the JSON data
+        for (const obj of jsonArray) {
+            const tests = obj.Texts;
 
-  try {
-    // Parse the file content into a JSON object
-    const jsonData = JSON.parse(data);
+            const isDecimalOrNumberWithRange = /^-?\d+(\.\d+)?|<[0-9]+|>[0-9]+$/;
+            tests.forEach((test,idx,array) => {
+                const testName = decodeURIComponent(test.R[0].T);
+                const next = array[idx + 1] || null;
 
-
-    // Regex patterns for extracting test name and result
-    const testNameRegex = /T":"([^"]+)/;
-    const resultRegex = /"S":(\d+)/;
-
-    const jsonArray = jsonData.Pages;
-
-    const oriTests = [
-        'WBC',
-        'RBC',
-        'Hemoglobin',
-        'Hematocrit',
-        'MCV',
-        'MCH', 
-        'MCHC',  
-        'RDW - CV',  
-        'Platelet',  
-        'Neutrophils', 
-        'Lymphocytes',  
-        'Monocytes',  
-        'Eosinophils',  
-        'Basophils - %',  
-        'Absolute Neutrophils',  
-        'Absolute Lymphocytes',  
-        'Absolute Monocytes',  
-        'Absolute Eosinophils',  
-        'Basophils',
-        'Glucose',
-        'BUN',
-        'Creatinine',
-        'eGFR (Non-African American)',
-        'eGFR (African American)',
-        'BUN/Creatinine Ratio',
-        'Sodium',
-        'Potassium',
-        'Chloride',
-        'Bicarbonate',
-        'Calcium',
-        'Total Protein',
-        'Albumin',
-        'Globulin',
-        'Albumin/Globulin Ratio',
-        'Total Bilirubin',
-        'Direct Bilirubin',
-        'AST (Aspartate aminotransferase)',
-        'ALT (Alanine aminotransferase)',
-        'ALP (Alkaline Phosphatase)',
-        'GGT',
-        'HbA1c, Glycosylated Hemoglobin',
-        'Insulin',
-        'Uric Acid',
-        'Cholesterol, Total',
-        'Triglycerides',
-        'HDL, Cholesterol',
-        'LDL Calculated',
-        'Cholesterol/HDL Ratio',
-        'Lipoprotein (a)',
-        'Homocysteine',
-        'C-Reactive Protein, Quant',
-        'TSH-DXI',
-        'Free T4 - DXI',
-        'Free T3 - DXI',
-        'Thyroglobulin Antibodies',
-        'Thyroid Peroxidase (TPO) Ab - DXI',
-        'Prolactin - DXI',
-        'LH - DXI',
-        'FSH - DXI',
-        'DHEA-Sulfate - DXI',
-        'Testosterone - DXI',
-        'Free Testosterone',
-        'Bioavailable Testosterone',
-        'SHBG',
-        'Estrone-MSS',
-        'Pregnenolone- MSS',
-        'Progesterone - DXI',
-        'IGF-1',
-        'Cortisol - DXI',
-        'Estradiol - DXI',
-        'Vitamin D - DXI',
-        'Ferritin',
-        'Vitamin B12, Diluted - DXI',
-        'Folate, Serum - DXI',
-    ];
-    // Iterate over the JSON data
-    for (const obj of jsonArray) {
-        const tests = obj.Texts;
-
-        const isDecimalOrNumberWithRange = /^-?\d+(\.\d+)?|<[0-9]+|>[0-9]+$/;
-        tests.forEach((test,idx,array) => {
-            const testName = decodeURIComponent(test.R[0].T);
-            const next = array[idx + 1] || null;
-
-            if ((/Result/).test(testName)) {
-                console.log("---------");
-            }
-            //console.log(testName);
-            if (oriTests.includes(testName)) {
-                const point = next.R[0].T;
-
-                if (isDecimalOrNumberWithRange.test(point)){
-                    console.log(`${testName} : ${point}`);
+                if (oriTests.includes(testName)) {
+                    const point = decodeURIComponent(next.R[0].T);
+                    if (isDecimalOrNumberWithRange.test(point) && !result.find(r => r.test === testName)){
+                        result.push({
+                            test: testName,
+                            value: point
+                        })
+                    }
                 }
-
-            }
-        });
-
-  
+            });
+        }
+    } catch (error) {
+        console.error('Error parsing JSON:', error);
     }
 
-  } catch (error) {
-    console.error('Error parsing JSON:', error);
-  }
-});
-
-};
+    return result;
+}
--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import { parsePdf } from './controllers/pdfController.js';
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.post('/parse-pdf',parsePdf);
+app.post('/parse-pdf', parsePdf);
 
 // Start the server
 app.listen(port, () => {
