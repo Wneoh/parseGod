f65c5ceb2913c9e642ea4adc99b6e91804d63d13
Wei Chin Neoh <weichin@gmail.com>
2023-09-26 02:55:03 +0800

    added authorize header

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import { parsePdf } from './controllers/pdfController.js';
 import bodyParser from 'body-parser';
@@ -8,9 +9,15 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  if (req.headers['bst-auth-token'] === process.env.APIKEYAUTH) {
+    next();
+  } else {
+    res.status(401).send('Unauthorized');
+  }
+});
 
 app.post('/parse-pdf', parsePdf);
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
